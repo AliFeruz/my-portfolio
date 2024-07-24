afc851ae2ccb52bc2c5e9ad0039478f41d802184
Ali Feruz <aliferuzcode@gmail.com>
2024-07-24 15:34:50 +0200

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex w-full h-[80px] items-center justify-center py-[16px] px-[299px] bg-black'>
+        <div className='h-[48px] flex justify-between items-center w-full px-[16px] rounded-[25px] bg-[#fff] flex-shrink-0'>
+            <div className='text-[#000] font-normal '>
+                Ali Feruz
+            </div>
+            <div className='flex space-x-4'>
+                <p>Work</p>
+                <p>About</p>
+                <p>Contact</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
